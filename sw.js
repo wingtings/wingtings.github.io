8fa1bb5ac06e31c1967ec63b38ffba2f48af44b0
wingtings <wuwt@mail.ustc.edu.cn>
2025-08-25 23:29:00 +0800

    Site updated: 2025-08-25 23:28:58

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1753621638264";const preCache = ["images/taichi.png","images/banner.webp","css/loader.css","css/style.css","js/script.js"];const cacheDomain = [
+const VERSION = "1756134867076";const preCache = ["images/taichi.png","images/banner.webp","css/loader.css","css/style.css","js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
